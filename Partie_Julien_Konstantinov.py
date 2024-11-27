37514dd9d1c1f01bdfe95e2eebdf4e3e196b44cf
JulienKonstantinov <julienkonstantinov@gmail.com>
2024-11-27 09:29:35 +0100

    Ajout fonction topics

--- Partie_Julien_Konstantinov.py
+++ Partie_Julien_Konstantinov.py
@@ -1,6 +1,8 @@
 import json
 import unicodedata
 import re
+from sklearn.feature_extraction.text import CountVectorizer
+import pandas as pd
 from bertopic import BERTopic
 
 
@@ -14,6 +16,7 @@ def file_open(path):
     with open(path,"r") as file:
         jason=json.load(file)
     return jason
+
 
 
 def text_cleaning(texte):
@@ -31,6 +34,8 @@ def text_cleaning(texte):
                                 "\U000024C2-\U0001F251]+", flags=re.UNICODE)
     texte_sans_emojis = pattern_emojis.sub(r'', texte_sans_retours)
     return texte_sans_emojis
+
+
 
 def special_caracters(jason):
     """
@@ -50,14 +55,15 @@ def special_caracters(jason):
 
 
 
-def author_ID(jason):
+def authors_list(jason):
     """
     Fonction permettant d'obtenir l'identification de l'auteur d'un tweet.
     """
+    author_list=[]
     length=len(jason)
     for i in range(0,length):
-        print("L'auteur du tweet n°"+str(i)+" est "+str(jason[i]["author_id"]))
-    return
+        author_list.append(jason[i]["author_id"])
+    return author_list
 
 
 
@@ -65,6 +71,7 @@ def hashtags_list(jason):
     """
     Fonction permettant d'obtenir la liste des hashtags présents dans un tweet. 
     """
+    hashtag_list=[]
     length=len(jason)
     for i in range(0,length):
         keys1=list(jason[i].keys())
@@ -72,15 +79,15 @@ def hashtags_list(jason):
             keys2=list(jason[i]["entities"].keys())
             if "hashtags" in keys2:
                 liste=jason[i]["entities"]["hashtags"]
-                liste2=[]
+                sous_liste=[]
                 for j in range(0,len(liste)):
-                    liste2.append("#"+liste[j]["tag"])
-                print("Tweet n°"+str(i)+" : "+str(liste2))
+                    sous_liste.append("#"+str(liste[j]["tag"]))
+                hashtag_list.append(str(sous_liste))
             else :
-                print("Tweet n°"+str(i)+" : Il n'y a pas de hashtags sur ce tweet")
+                hashtag_list.append([])
         else:
-            print("Tweet n°"+str(i)+" : Il n'y a pas d'entité sur ce tweet")
-    return
+            hashtag_list.append([])
+    return hashtag_list
     
 
 
@@ -88,6 +95,7 @@ def users_list(jason):
     """
     Fonction permettant d'obtenir la liste des utilisateurs mentionnés dans un tweet. 
     """
+    user_list=[]
     length=len(jason)
     for i in range(0,length):
         keys1=list(jason[i].keys())
@@ -95,50 +103,97 @@ def users_list(jason):
             keys2=list(jason[i]["entities"].keys())
             if "mentions" in keys2:
                 liste=jason[i]["entities"]["mentions"]
-                liste2=[]
+                sous_liste=[]
                 for j in range(0,len(liste)):
-                    liste2.append("@"+liste[j]["username"])
-                print("Tweet n°"+str(i)+" : "+str(liste2))
+                    sous_liste.append("@"+liste[j]["username"])
+                user_list.append(str(sous_liste))
             else :
-                print("Tweet n°"+str(i)+" : Il n'y a pas d'utilisateurs mentionnés sur ce tweet")
+                user_list.append([])
         else:
-            print("Tweet n°"+str(i)+" : Il n'y a pas d'entité sur ce tweet")
-    return
+            user_list.append([])
+    return user_list
+
+
+#def top_K_hashtags(k,df):
 
 
 
-def topics(jason):
 
-    with open('Topics_list.json') as file:
-        topics_dico=json.load(file)
+# def topics(jason):
 
-    model = BERTopic()
-    print("Domaines détectés pour chaque phrase :")
-    length=len(jason)
-    for i in range(0,length):
-        topic = model.fit_transform(jason[i]["text"])
-        domain = topics_dico.get(topic, "Inconnu")  
-        print(f"Phrase : {jason[i]["text"]}")
-        print(f" - Sujet détecté : {topic}")
-        print(f" - Domaine général : {domain}")
-        print()
+#     with open('topics_dict.json') as file:
+#         topics_dico=json.load(file)
 
-    print("Détails sur les sujets et mots-clés associés :")
-    for topic_id in set(topics):
-        print(f"Sujet {topic_id} : {model.get_topic(topic_id)}")
-    return
+#     model = BERTopic()
+#     print("Domaines détectés pour chaque phrase :")
+#     length=len(jason)
+#     for i in range(0,length):
+#         topic = model.fit_transform(jason[i]["text"])
+#         domain = topics_dico.get(topic, "Inconnu")  
+#         print(f"Phrase : {jason[i]["text"]}")
+#         print(f" - Sujet détecté : {topic}")
+#         print(f" - Domaine général : {domain}")
+#         print()
+
+#     print("Détails sur les sujets et mots-clés associés :")
+#     for topic_id in set(topics):
+#         print(f"Sujet {topic_id} : {model.get_topic(topic_id)}")
+#     return
 
 
+def train_topic_model(phrases_dict):
+    """
+    Entraîne un modèle BERTopic basé sur les phrases du dictionnaire.
+    Retourne le modèle et les résultats de l'entraînement.
+    """
+    phrases = list(phrases_dict.values())
+    
+    # Initialisation d'un modèle BERTopic avec un vectoriseur optimisé
+    vectorizer_model = CountVectorizer(stop_words="english", max_df=0.9)
+    topic_model = BERTopic(vectorizer_model=vectorizer_model)
+    
+    # Entraîne le modèle
+    topics, probs = topic_model.fit_transform(phrases)
+    
+    return topic_model, topics, probs
+
+def find_topic_for_new_phrase(topic_model, new_phrase, topics_dict):
+    """
+    Utilise un modèle BERTopic pour trouver le sujet le plus proche d'une nouvelle phrase.
+    Retourne l'identifiant et le libellé du sujet.
+    """
+    topic_id = topic_model.transform([new_phrase])[0][0]  # Récupère l'ID du sujet
+    topic_name = topics_dict.get(str(topic_id), "Sujet inconnu")
+    return topic_id, topic_name
+
+
+    
 
 def main(path):
+    data={}
     jason=file_open(path)
     clean_text=special_caracters(jason)
-    print(clean_text)
-    print(author_ID(jason))
-    print(hashtags_list(jason))
-    print(users_list(jason))
-    print(topics(jason))
+    data["Auteur"]=authors_list(jason)
+    data["Hashtags"]=hashtags_list(jason)
+    data["Mentions"]=users_list(jason)
+    #print(topics(jason))
+    df=pd.DataFrame(data)
+    compte_authors=df["Auteur"].value_counts()
+    compte_hashtags=df["Hashtags"].value_counts()
+    compte_mentions=df["Mentions"].value_counts()
 
+    topics_file = "topics_dict.json"  # Fichier contenant les sujets (clé: ID, valeur: nom du sujet)
+    phrases_file = "sentences_dict.json"  # Fichier contenant les phrases associées aux sujets
+    topics_dict = file_open(topics_file)
+    phrases_dict = file_open(phrases_file)
+    print("Entraînement du modèle BERTopic...")
+    topic_model, topics, probs = train_topic_model(phrases_dict)
+    print("Modèle entraîné avec succès.")
+    new_phrase = input("Entrez une nouvelle phrase pour identifier son sujet : ")
+    topic_id, topic_name = find_topic_for_new_phrase(topic_model, new_phrase, topics_dict)
+    
+    print(f"Sujet identifié : {topic_name} (ID: {topic_id})")
+   
     return
 
 
