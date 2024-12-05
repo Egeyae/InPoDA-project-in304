71f7e8432457cf3ea9466a4824f514e5a10a98b7
Julien Konstantinov <julienkonstantinov@gmail.com>
2024-12-05 22:58:51 +0100

    Fonction topics + quasi clean dataframe

--- Partie_Julien_Konstantinov.py
+++ Partie_Julien_Konstantinov.py
@@ -1,9 +1,9 @@
 import json
 import unicodedata
 import re
-from sklearn.feature_extraction.text import CountVectorizer
 import pandas as pd
-from bertopic import BERTopic
+from transformers import pipeline, XLMRobertaTokenizer
+import spacy
 
 
 def file_open(path):
@@ -25,15 +25,19 @@ def text_cleaning(texte):
     """
     texte_normalise = unicodedata.normalize('NFD', texte)
     texte_sans_accents = ''.join(c for c in texte_normalise if unicodedata.category(c) != 'Mn')
-    texte_sans_retours = texte_sans_accents.replace('\n', '').replace('\r', '').replace("'", '') .replace("’", '').replace("\u200d",'')
     pattern_emojis = re.compile("[\U0001F600-\U0001F64F\U0001F300-\U0001F5FF"
                                 "\U0001F680-\U0001F6FF\U0001F700-\U0001F77F"
                                 "\U0001F780-\U0001F7FF\U0001F800-\U0001F8FF"
                                 "\U0001F900-\U0001F9FF\U0001FA00-\U0001FA6F"
                                 "\U0001FA70-\U0001FAFF\U00002702-\U000027B0"
                                 "\U000024C2-\U0001F251]+", flags=re.UNICODE)
-    texte_sans_emojis = pattern_emojis.sub(r'', texte_sans_retours)
-    return texte_sans_emojis
+    texte_sans_emojis = pattern_emojis.sub(r'', texte_sans_accents)
+    texte_sans_lien = re.sub(r"https?://\S+", "", texte_sans_emojis)
+    texte_sans_arobases = re.sub(r"@\w+", "", texte_sans_lien)
+    texte_sans_hashtags = re.sub(r"#\w+", "", texte_sans_arobases)
+    texte_sans_parasites = texte_sans_hashtags.replace('\n', '').replace('\r', '').replace("'", '').replace("’", '').replace("\u200d",'').replace("@",'').replace("#",'')
+    texte_final = re.sub(r"\s+", " ", texte_sans_parasites).strip()
+    return texte_final
 
 
 
@@ -95,6 +99,7 @@ def users_list(jason):
     """
     Fonction permettant d'obtenir la liste des utilisateurs mentionnés dans un tweet. 
     """
+    df = pd.DataFrame(columns=["Users"])
     user_list=[]
     length=len(jason)
     for i in range(0,length):
@@ -114,60 +119,97 @@ def users_list(jason):
     return user_list
 
 
-#def top_K_hashtags(k,df):
+def start_model():
+    model_name = "joeddav/xlm-roberta-large-xnli"
+    tokenizer = XLMRobertaTokenizer.from_pretrained(model_name)
+    classifier = pipeline("zero-shot-classification", model=model_name, tokenizer=tokenizer)
+
+    return classifier
 
 
+def topics(jason,classifier,k=5):
+
+    topic_list=[]
+    score_list=[]
+
+    labels = ["Physique", "Chimie", "Biologie", "Astronomie", "Géologie", 
+              "Climatologie", "Océanographie", "Écologie", "Zoologie", "Botanique", 
+              "Génétique", "Biotechnologie", "Histoire", "Géographie", "Sociologie", 
+              "Anthropologie", "Archéologie", "Psychologie", "Sciences politiques", 
+              "Économie", "Démographie", "Philosophie", "Éthique", "Linguistique", 
+              "Études culturelles", "Ingénierie civile", "Ingénierie mécanique", 
+              "Ingénierie électrique", "Ingénierie informatique", "Ingénierie aérospatiale", 
+              "Ingénierie chimique", "Robotique", "Intelligence artificielle", "Blockchain", 
+              "Cybersécurité", "Internet des objets", "Nanotechnologie", "Médecine générale", 
+              "Médecine dentaire", "Pharmacie", "Infirmerie", "Nutrition", "Kinésithérapie", 
+              "Chirurgie", "Psychiatrie", "Pédiatrie", "Médecine vétérinaire", "Santé publique", 
+              "Neurosciences", "Mathématiques", "Statistiques", "Informatique", "Logique", 
+              "Théorie des jeux", "Recherche opérationnelle", "Cryptographie", "Littérature", 
+              "Arts visuels", "Musique", "Théâtre", "Cinéma", "Design", "Architecture", 
+              "Études religieuses", "Histoire de l'art", "Poésie", "Finance", "Comptabilité", 
+              "Marketing", "Gestion des ressources humaines", "Entrepreneuriat", "Commerce international", 
+              "Logistique", "Stratégie d’entreprise", "Économie comportementale", 
+              "Économie verte", "Droit civil", "Droit pénal", "Droit commercial", 
+              "Droit du travail", "Droit international", "Droit de la propriété intellectuelle", 
+              "Droit des technologies", "Criminologie", "Médiation juridique", "Pédagogie", 
+              "Didactique", "Éducation spécialisée", "Formation continue", "Psychologie de l’éducation", 
+              "Politique éducative", "Journalisme", "Relations publiques", "Publicité", 
+              "Cinématographie", "Rédaction technique", "Études médiatiques", "Communication numérique", 
+              "Réseaux sociaux", "Agriculture", "Agronomie", "Sylviculture", "Horticulture", 
+              "Agroécologie", "Gestion des ressources naturelles", "Énergies renouvelables", 
+              "Développement durable", "Pollution et gestion des déchets", "Entraînement sportif", 
+              "Kinésiologie", "Médecine du sport", "Tourisme", "Hôtellerie", "Gastronomie", 
+              "Jeux vidéo", "Esports", "Sécurité informatique", "Sécurité nationale", 
+              "Politiques de défense", "Renseignement", "Gestion des catastrophes", 
+              "Sécurité privée", "Droit international humanitaire", "Études interdisciplinaires", 
+              "Science des données", "Sciences des matériaux", "Études sur le genre", 
+              "Études sur les minorités", "Recherche scientifique", "Innovation sociale"]
+    
+    length=len(jason)
+    for i in range(0,length):
+        cleaned_text=text_cleaning(jason[i]["text"])
+        text=cleaned_text
+        print(text)
+        if text:
+            result = classifier(text, candidate_labels=labels)
+            result = zip(result["labels"],result["scores"])
+            result = sorted(result, key=lambda x: x[1], reverse=True)
+            #print(result[:k])
+            topic_list.append(result[:k][0][0])
+            score_list.append(result[:k][0][1])
+        else:
+            #print("None")
+            topic_list.append("None")
+            score_list.append("None")
 
 
-# def topics(jason):
+    return topic_list, score_list
 
-#     with open('topics_dict.json') as file:
-#         topics_dico=json.load(file)
+# def topics():
 
-#     model = BERTopic()
-#     print("Domaines détectés pour chaque phrase :")
-#     length=len(jason)
-#     for i in range(0,length):
-#         topic = model.fit_transform(jason[i]["text"])
-#         domain = topics_dico.get(topic, "Inconnu")  
-#         print(f"Phrase : {jason[i]["text"]}")
-#         print(f" - Sujet détecté : {topic}")
-#         print(f" - Domaine général : {domain}")
-#         print()
-
-#     print("Détails sur les sujets et mots-clés associés :")
-#     for topic_id in set(topics):
-#         print(f"Sujet {topic_id} : {model.get_topic(topic_id)}")
+#     nlp = spacy.load('fr_core_news_sm')
+#     sentence = "Emmanuel Macron a parlé du réchauffement climatique."
+#     doc = nlp(sentence)
+#     for token in doc:
+#         if 'subj' in token.dep_:
+#             print("Sujet trouvé :", token.text)
+    
 #     return
 
-
-def train_topic_model(phrases_dict):
-    """
-    Entraîne un modèle BERTopic basé sur les phrases du dictionnaire.
-    Retourne le modèle et les résultats de l'entraînement.
-    """
-    phrases = list(phrases_dict.values())
-    
-    # Initialisation d'un modèle BERTopic avec un vectoriseur optimisé
-    vectorizer_model = CountVectorizer(stop_words="english", max_df=0.9)
-    topic_model = BERTopic(vectorizer_model=vectorizer_model)
-    
-    # Entraîne le modèle
-    topics, probs = topic_model.fit_transform(phrases)
-    
-    return topic_model, topics, probs
-
-def find_topic_for_new_phrase(topic_model, new_phrase, topics_dict):
-    """
-    Utilise un modèle BERTopic pour trouver le sujet le plus proche d'une nouvelle phrase.
-    Retourne l'identifiant et le libellé du sujet.
-    """
-    topic_id = topic_model.transform([new_phrase])[0][0]  # Récupère l'ID du sujet
-    topic_name = topics_dict.get(str(topic_id), "Sujet inconnu")
-    return topic_id, topic_name
+def dataframe_analysis(df,K):
+    compte_authors=df["Auteur"].value_counts()
+    max_authors=compte_authors.nlargest(K)
+    print(f"Les {K} utilisateurs les plus actifs sont : {max_authors}.")
+    compte_hashtags=df["Hashtags"].value_counts()
+    compte_mentions=df["Mentions"].value_counts()
+    compte_topics=df["Topics"].value_counts()
+    max_topics=compte_topics.nlargest(K)
+    print(f"Les {K} sujets les plus récurrents sont : {max_topics}.")
 
 
-    
+
+
+
 
 def main(path):
     data={}
@@ -176,27 +218,15 @@ def main(path):
     data["Auteur"]=authors_list(jason)
     data["Hashtags"]=hashtags_list(jason)
     data["Mentions"]=users_list(jason)
-    #print(topics(jason))
+    classifier=start_model()
+    data["Topics"]=topics(jason,classifier,k=5)[0]
+    data["Scores"]=topics(jason,classifier,k=5)[1]
     df=pd.DataFrame(data)
-    compte_authors=df["Auteur"].value_counts()
-    compte_hashtags=df["Hashtags"].value_counts()
-    compte_mentions=df["Mentions"].value_counts()
-
-    topics_file = "topics_dict.json"  # Fichier contenant les sujets (clé: ID, valeur: nom du sujet)
-    phrases_file = "sentences_dict.json"  # Fichier contenant les phrases associées aux sujets
-    topics_dict = file_open(topics_file)
-    phrases_dict = file_open(phrases_file)
-    print("Entraînement du modèle BERTopic...")
-    topic_model, topics, probs = train_topic_model(phrases_dict)
-    print("Modèle entraîné avec succès.")
-    new_phrase = input("Entrez une nouvelle phrase pour identifier son sujet : ")
-    new_phrase = "".join([x for x in new_phrase.split() if not x.startswith("#")])
-    topic_id, topic_name = find_topic_for_new_phrase(topic_model, new_phrase, topics_dict)
-    
-    print(f"Sujet identifié : {topic_name} (ID: {topic_id})")
+    print(dataframe_analysis(df,4))
    
     return
 
 
 print(main('versailles_tweets_100.json'))
+
 
