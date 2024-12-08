b22d1df63df3188892ee6bc72ab3da9edf5c94c8
Egeya <cossec.elouan@gmail.com>
2024-12-08 14:39:30 +0100

    Work

--- Partie_Julien_Konstantinov.py
+++ Partie_Julien_Konstantinov.py
@@ -12,13 +12,12 @@ def file_open(path):
     """
     Fonction qui permet d'ouvrir un fichier 
     """
-    if type(path)!=str:
+    if type(path) != str:
         print("Erreur sur le type du chemin")
 
-    with open(path,"r") as file:
-        jason=json.load(file)
+    with open(path, "r", encoding="utf-8", errors="replace") as file:
+        jason = json.load(file)
     return jason
-
 
 
 def text_cleaning(texte):
@@ -37,64 +36,62 @@ def text_cleaning(texte):
     texte_sans_lien = re.sub(r"https?://\S+", "", texte_sans_emojis)
     texte_sans_arobases = re.sub(r"@\w+", "", texte_sans_lien)
     texte_sans_hashtags = re.sub(r"#\w+", "", texte_sans_arobases)
-    texte_sans_parasites = texte_sans_hashtags.replace('\n', '').replace('\r', '').replace("'", '').replace("’", '').replace("\u200d",'').replace("@",'').replace("#",'')
+    texte_sans_parasites = texte_sans_hashtags.replace('\n', '').replace('\r', '').replace("'", '').replace("’",
+                                                                                                            '').replace(
+        "\u200d", '').replace("@", '').replace("#", '')
     texte_final = re.sub(r"\s+", " ", texte_sans_parasites).strip()
     return texte_final
-
 
 
 def special_caracters(jason):
     """
     Fonction qui permet de stocker les tweets sans caractères spéciaux dans un fichier JSON.
     """
-    length=len(jason)
-    dico={}
-    for i in range(0,length):
-        if jason[i]["text"].isalnum()==True:
-            dico[i]=jason[i]["text"]
+    length = len(jason)
+    dico = {}
+    for i in range(0, length):
+        if jason[i]["text"].isalnum() == True:
+            dico[i] = jason[i]["text"]
         else:
-            dico[i]=text_cleaning(jason[i]["text"])
-    
-    with open("zone_d'atterissage.json",'w') as file2:
-        json.dump(dico,file2,indent=2)
-    return dico
+            dico[i] = text_cleaning(jason[i]["text"])
 
+    with open("zone_d'atterissage.json", 'w') as file2:
+        json.dump(dico, file2, indent=2)
+    return dico
 
 
 def authors_list(jason):
     """
     Fonction permettant d'obtenir l'identification de l'auteur d'un tweet.
     """
-    author_list=[]
-    length=len(jason)
-    for i in range(0,length):
+    author_list = []
+    length = len(jason)
+    for i in range(0, length):
         author_list.append(jason[i]["author_id"])
     return author_list
-
 
 
 def hashtags_list(jason):
     """
     Fonction permettant d'obtenir la liste des hashtags présents dans un tweet. 
     """
-    hashtag_list=[]
-    length=len(jason)
-    for i in range(0,length):
-        keys1=list(jason[i].keys())
+    hashtag_list = []
+    length = len(jason)
+    for i in range(0, length):
+        keys1 = list(jason[i].keys())
         if "entities" in keys1:
-            keys2=list(jason[i]["entities"].keys())
+            keys2 = list(jason[i]["entities"].keys())
             if "hashtags" in keys2:
-                liste=jason[i]["entities"]["hashtags"]
-                sous_liste=[]
-                for j in range(0,len(liste)):
-                    sous_liste.append("#"+str(liste[j]["tag"]))
+                liste = jason[i]["entities"]["hashtags"]
+                sous_liste = []
+                for j in range(0, len(liste)):
+                    sous_liste.append("#" + str(liste[j]["tag"]))
                 hashtag_list.append(str(sous_liste))
-            else :
+            else:
                 hashtag_list.append([])
         else:
             hashtag_list.append([])
     return hashtag_list
-    
 
 
 def users_list(jason):
@@ -102,88 +99,99 @@ def users_list(jason):
     Fonction permettant d'obtenir la liste des utilisateurs mentionnés dans un tweet. 
     """
     df = pd.DataFrame(columns=["Users"])
-    user_list=[]
-    length=len(jason)
-    for i in range(0,length):
-        keys1=list(jason[i].keys())
+    user_list = []
+    length = len(jason)
+    for i in range(0, length):
+        keys1 = list(jason[i].keys())
         if "entities" in keys1:
-            keys2=list(jason[i]["entities"].keys())
+            keys2 = list(jason[i]["entities"].keys())
             if "mentions" in keys2:
-                liste=jason[i]["entities"]["mentions"]
-                sous_liste=[]
-                for j in range(0,len(liste)):
-                    sous_liste.append("@"+liste[j]["username"])
+                liste = jason[i]["entities"]["mentions"]
+                sous_liste = []
+                for j in range(0, len(liste)):
+                    sous_liste.append("@" + liste[j]["username"])
                 user_list.append(str(sous_liste))
-            else :
+            else:
                 user_list.append([])
         else:
             user_list.append([])
     return user_list
 
 
+def contenu_list(jason):
+    contenus = []
+    length = len(jason)
+    for i in range(0, length):
+        contenus.append(jason[i]["text"])
+    return contenus
+
+
 def start_model():
-    model_name = "joeddav/xlm-roberta-large-xnli"
+    models = {
+        "small": "xlm-roberta-base",
+        "big": "joeddav/xlm-roberta-large-xnli"  # Warning ! This is very slow
+    }
+    model_name = models["small"]
     tokenizer = XLMRobertaTokenizer.from_pretrained(model_name)
     classifier = pipeline("zero-shot-classification", model=model_name, tokenizer=tokenizer)
 
     return classifier
 
 
-def topics(jason,classifier,k=5):
+def topics(jason, classifier, k=5):
+    topic_list = []
+    score_list = []
 
-    topic_list=[]
-    score_list=[]
-
-    labels = ["Physique", "Chimie", "Biologie", "Astronomie", "Géologie", 
-              "Climatologie", "Océanographie", "Écologie", "Zoologie", "Botanique", 
-              "Génétique", "Biotechnologie", "Histoire", "Géographie", "Sociologie", 
-              "Anthropologie", "Archéologie", "Psychologie", "Sciences politiques", 
-              "Économie", "Démographie", "Philosophie", "Éthique", "Linguistique", 
-              "Études culturelles", "Ingénierie civile", "Ingénierie mécanique", 
-              "Ingénierie électrique", "Ingénierie informatique", "Ingénierie aérospatiale", 
-              "Ingénierie chimique", "Robotique", "Intelligence artificielle", "Blockchain", 
-              "Cybersécurité", "Internet des objets", "Nanotechnologie", "Médecine générale", 
-              "Médecine dentaire", "Pharmacie", "Infirmerie", "Nutrition", "Kinésithérapie", 
-              "Chirurgie", "Psychiatrie", "Pédiatrie", "Médecine vétérinaire", "Santé publique", 
-              "Neurosciences", "Mathématiques", "Statistiques", "Informatique", "Logique", 
-              "Théorie des jeux", "Recherche opérationnelle", "Cryptographie", "Littérature", 
-              "Arts visuels", "Musique", "Théâtre", "Cinéma", "Design", "Architecture", 
-              "Études religieuses", "Histoire de l'art", "Poésie", "Finance", "Comptabilité", 
-              "Marketing", "Gestion des ressources humaines", "Entrepreneuriat", "Commerce international", 
-              "Logistique", "Stratégie d’entreprise", "Économie comportementale", 
-              "Économie verte", "Droit civil", "Droit pénal", "Droit commercial", 
-              "Droit du travail", "Droit international", "Droit de la propriété intellectuelle", 
-              "Droit des technologies", "Criminologie", "Médiation juridique", "Pédagogie", 
-              "Didactique", "Éducation spécialisée", "Formation continue", "Psychologie de l’éducation", 
-              "Politique éducative", "Journalisme", "Relations publiques", "Publicité", 
-              "Cinématographie", "Rédaction technique", "Études médiatiques", "Communication numérique", 
-              "Réseaux sociaux", "Agriculture", "Agronomie", "Sylviculture", "Horticulture", 
-              "Agroécologie", "Gestion des ressources naturelles", "Énergies renouvelables", 
-              "Développement durable", "Pollution et gestion des déchets", "Entraînement sportif", 
-              "Kinésiologie", "Médecine du sport", "Tourisme", "Hôtellerie", "Gastronomie", 
-              "Jeux vidéo", "Esports", "Sécurité informatique", "Sécurité nationale", 
-              "Politiques de défense", "Renseignement", "Gestion des catastrophes", 
-              "Sécurité privée", "Droit international humanitaire", "Études interdisciplinaires", 
-              "Science des données", "Sciences des matériaux", "Études sur le genre", 
+    labels = ["Physique", "Chimie", "Biologie", "Astronomie", "Géologie",
+              "Climatologie", "Océanographie", "Écologie", "Zoologie", "Botanique",
+              "Génétique", "Biotechnologie", "Histoire", "Géographie", "Sociologie",
+              "Anthropologie", "Archéologie", "Psychologie", "Sciences politiques",
+              "Économie", "Démographie", "Philosophie", "Éthique", "Linguistique",
+              "Études culturelles", "Ingénierie civile", "Ingénierie mécanique",
+              "Ingénierie électrique", "Ingénierie informatique", "Ingénierie aérospatiale",
+              "Ingénierie chimique", "Robotique", "Intelligence artificielle", "Blockchain",
+              "Cybersécurité", "Internet des objets", "Nanotechnologie", "Médecine générale",
+              "Médecine dentaire", "Pharmacie", "Infirmerie", "Nutrition", "Kinésithérapie",
+              "Chirurgie", "Psychiatrie", "Pédiatrie", "Médecine vétérinaire", "Santé publique",
+              "Neurosciences", "Mathématiques", "Statistiques", "Informatique", "Logique",
+              "Théorie des jeux", "Recherche opérationnelle", "Cryptographie", "Littérature",
+              "Arts visuels", "Musique", "Théâtre", "Cinéma", "Design", "Architecture",
+              "Études religieuses", "Histoire de l'art", "Poésie", "Finance", "Comptabilité",
+              "Marketing", "Gestion des ressources humaines", "Entrepreneuriat", "Commerce international",
+              "Logistique", "Stratégie d’entreprise", "Économie comportementale",
+              "Économie verte", "Droit civil", "Droit pénal", "Droit commercial",
+              "Droit du travail", "Droit international", "Droit de la propriété intellectuelle",
+              "Droit des technologies", "Criminologie", "Médiation juridique", "Pédagogie",
+              "Didactique", "Éducation spécialisée", "Formation continue", "Psychologie de l’éducation",
+              "Politique éducative", "Journalisme", "Relations publiques", "Publicité",
+              "Cinématographie", "Rédaction technique", "Études médiatiques", "Communication numérique",
+              "Réseaux sociaux", "Agriculture", "Agronomie", "Sylviculture", "Horticulture",
+              "Agroécologie", "Gestion des ressources naturelles", "Énergies renouvelables",
+              "Développement durable", "Pollution et gestion des déchets", "Entraînement sportif",
+              "Kinésiologie", "Médecine du sport", "Tourisme", "Hôtellerie", "Gastronomie",
+              "Jeux vidéo", "Esports", "Sécurité informatique", "Sécurité nationale",
+              "Politiques de défense", "Renseignement", "Gestion des catastrophes",
+              "Sécurité privée", "Droit international humanitaire", "Études interdisciplinaires",
+              "Science des données", "Sciences des matériaux", "Études sur le genre",
               "Études sur les minorités", "Recherche scientifique", "Innovation sociale"]
-    
-    length=len(jason)
-    for i in range(0,length):
-        cleaned_text=text_cleaning(jason[i]["text"])
-        text=cleaned_text
-        print(f"Tweet n°{i} analysé.")
-        if text:
-            result = classifier(text, candidate_labels=labels)
-            result = zip(result["labels"],result["scores"])
+
+    length = len(jason)
+
+    texts = [text_cleaning(jason[i]["text"]) for i in range(length)]
+
+    for i, txt in enumerate(texts):
+        if txt:
+            result = classifier(txt, candidate_labels=labels)
+            result = zip(result["labels"], result["scores"])
             result = sorted(result, key=lambda x: x[1], reverse=True)
             topic_list.append(result[:k][0][0])
             score_list.append(result[:k][0][1])
         else:
             topic_list.append("None")
             score_list.append("None")
-
-
+        print(f"Tweet n°{i} analysé")
     return topic_list, score_list
+
 
 # def topics():
 
@@ -193,65 +201,70 @@ def topics(jason,classifier,k=5):
 #     for token in doc:
 #         if 'subj' in token.dep_:
 #             print("Sujet trouvé :", token.text)
-    
+
 #     return
 
 def column_to_list(liste):
     liste_plate = []
     for element in liste:
         if isinstance(element, str) and element.startswith("["):
-            sous_liste = ast.literal_eval(element) 
-            liste_plate.extend(sous_liste) 
+            sous_liste = ast.literal_eval(element)
+            liste_plate.extend(sous_liste)
         elif isinstance(element, list):
             liste_plate.extend(element)
     return liste_plate
 
 
-def dataframe_analysis(df,K):
+def dataframe_analysis(df, K):
     #Auteurs
-    compte_authors=df["Auteur"].value_counts()
-    max_authors=compte_authors.nlargest(K)
+    compte_authors = df["Auteur"].value_counts()
+    max_authors = compte_authors.nlargest(K)
     print(f"Les {K} utilisateurs les plus actifs sont : {max_authors}.")
     print(f"Nombre de publications par auteurs : {compte_authors}")
     #Hashtags
-    compte_hashtags=df["Hashtags"].value_counts()
-    dataframe_list=df["Hashtags"].to_list()
-    hashtags_list=column_to_list(dataframe_list)
+    compte_hashtags = df["Hashtags"].value_counts()
+    dataframe_list = df["Hashtags"].to_list()
+    hashtags_list = column_to_list(dataframe_list)
     compteur = Counter(hashtags_list)
     max_hashtags = compteur.most_common(K)
     print(f"Les {K} hashtags les plus fréquents sont {max_hashtags}")
     for element, count in compteur.items():
         print(f"Nombre de publications par hashtags : {element} : {count}")
     #Mentions
-    compte_mentions=df["Mentions"].value_counts()
-    dataframe_list_2=df["Mentions"].to_list()
-    mentions_list=column_to_list(dataframe_list_2)
+    compte_mentions = df["Mentions"].value_counts()
+    dataframe_list_2 = df["Mentions"].to_list()
+    mentions_list = column_to_list(dataframe_list_2)
     compteur2 = Counter(mentions_list)
     max_mentions = compteur2.most_common(K)
     print(f"Les {K} auteurs les plus mentionnés sont {max_mentions}")
     #Topics
-    compte_topics=df["Topics"].value_counts()
-    max_topics=compte_topics.nlargest(K)
+    compte_topics = df["Topics"].value_counts()
+    max_topics = compte_topics.nlargest(K)
     print(f"Les {K} sujets les plus récurrents sont : {max_topics}.")
     print(f"Nombre de publications par topics : {compte_topics}")
 
 
+def tweets_to_df(tweets):
+    data = {
+        "Auteur": authors_list(tweets),
+        "Hashtags": hashtags_list(tweets),
+        "Mentions": users_list(tweets),
+        "Contenu": contenu_list(tweets)
+    }
+    classifier = start_model()
+    data["Topics"] = topics(tweets, classifier, k=5)[0]
+    return pd.DataFrame(data)
+
 
 def main(path):
-    data={}
-    jason=file_open(path)
-    clean_text=special_caracters(jason)
-    data["Auteur"]=authors_list(jason)
-    data["Hashtags"]=hashtags_list(jason)
-    data["Mentions"]=users_list(jason)
-    classifier=start_model()
-    data["Topics"]=topics(jason,classifier,k=5)[0]
-    df=pd.DataFrame(data)
-    print(dataframe_analysis(df,1))
-   
+    data = {}
+    jason = file_open(path)
+    clean_text = special_caracters(jason)
+    df = tweets_to_df(jason)
+    print(dataframe_analysis(df, 1))
+
     return
 
 
-print(main('versailles_tweets_100.json'))
-
-
+if __name__ == "__main__":
+    print(main('versailles_tweets_100.json'))
